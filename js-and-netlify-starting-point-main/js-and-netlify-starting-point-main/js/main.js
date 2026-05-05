ef4eac4352a54c1c85fb9d7d90a596f7abc11be1
Astraeus07 <Astraeus.o70@gmail.com>
2026-05-05 01:12:35 -0500

    yuck

---
+++ js-and-netlify-starting-point-main/js-and-netlify-starting-point-main/js/main.js
@@ -0,0 +1,20 @@
+const x= "full-menu" ;
+const y= "show-menu";
+const z= "close-btn";
+
+show-menu.addEventListener("click",  "/full-menu");
+
+document.getElementById("show-menu");
+document.getElementById("full-menu");
+
+show-menu.addEventListener("click", ) ;
+
+fullMenu.classList.add("show");
+
+  document.getElementById('close-btn');
+  document.getElementById('full-menu');
+
+  closeButton.addEventListener('click', function() {
+    
+    fullMenu.classList.remove('show-menu');
+});
